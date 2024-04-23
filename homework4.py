3a40bed590e6c60571d19624507ae2eff551da6e
stels24 <166283589+stels24@users.noreply.github.com>
2024-04-23 21:46:26 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+immutable_var = 0, 1, 3, 6, "Браво!", True
+print(immutable_var)
+#immutable_var[3] = 15 # не изменяемые значения в кортедже
+
+mutable_list = ["apple", "strawberry", "rose"]
+print(mutable_list)
+mutable_list[2] = "chamomile"
+print(mutable_list)
+mutable_list[0] = "orange"
+print(mutable_list)
